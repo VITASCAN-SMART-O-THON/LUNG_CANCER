b1c469dac586d757204238116f41bb4d61f3173c
VITASCAN-SMART-O-THON <137748780+VITASCAN-SMART-O-THON@users.noreply.github.com>
2023-06-26 11:17:22 +0530

    Update Go Back To Home.py

--- WEB/pages/Go Back To Home.py
+++ WEB/pages/Go Back To Home.py
@@ -8,6 +8,7 @@ st.header('Are you sure want to leave this page?')
 
 
 url = 'https://stackoverflow.com'
+url1 = 'https://vitascan-smart-o-thon-lung-cancer-webapp-htnx26.streamlit.app/'
 st.markdown(f'''
 <a href={url}><button style="background-color:White;  height: 2em; width: 18em; font-size:30px;">Yes</button></a>
 ''',
@@ -15,6 +16,6 @@ unsafe_allow_html=True)
 
 
 st.markdown(f'''
-<a href={url}><button style="background-color:White;height: 2em; font-size:30px; width: 18em">No</button></a>
+<a href={url1}><button style="background-color:White;height: 2em; font-size:30px; width: 18em">No</button></a>
 ''',
 unsafe_allow_html=True)
